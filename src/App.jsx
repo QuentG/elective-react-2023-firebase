080f79fe8cd0b711ea75551cacc6fa793132f718
Quentin Gans <contact@quentingans.fr>
2023-02-16 15:38:51 +0100

    Add firebase auth

--- src/App.jsx
+++ src/App.jsx
@@ -1,57 +1,30 @@
-import React, { useEffect, useState } from 'react'
-import { addDoc, collection, getDocs } from 'firebase/firestore'
+import React from 'react'
+import { Route, Routes } from 'react-router-dom'
 import './App.css'
-import { db } from './firebase'
-import Todo from './components/Todo'
+import LoginForm from './components/LoginForm'
+import RegisterForm from './components/RegisterForm'
+import Layout from './pages/Layout'
+import Home from './pages/Home'
+import Todos from './pages/Todos'
+import AuthProvider from './provider/AuthProvider'
+import ProtectedRoute from './routes/ProtectedRoute'
 
 const App = () => {
-  const [todos, setTodos] = useState([])
-  const [todoName, setTodoName] = useState('')
-
-  const getTodos = async () => {
-    const colect = collection(db, 'todos')
-    const response = await getDocs(colect)
-    const data = response.docs.map(doc => ({ id: doc.id, ...doc.data() }))
-
-    setTodos(data)
-  }
-
-  useEffect(() => {
-    getTodos()
-  }, [todos])
-
-  const onAddTodo = async (e) => {
-    e.preventDefault()
-
-    try {
-      const colect = collection(db, 'todos')
-      const doc = await addDoc(colect, {
-        name: todoName,
-        completed: false
-      })
-
-      console.log('Document written with ID -> ', doc.id)
-    } catch (error) {
-      console.error('Error adding document -> ', error)
-    }
-
-    setTodoName('')
-  }
-
   return (
-    <div className="App">
-      <h1>Firebase Demo</h1>
-
-      <form onSubmit={onAddTodo}>
-        <input type="text" value={todoName} onChange={e => setTodoName(e.target.value)} />
-        <button type="submit">Ajouter la todo</button>
-      </form>
-  
-      {todos.length ? todos.map(todo => (
-        <Todo key={todo.id} todo={todo} />
-      )) : <span>Aucune todo</span>}
-
-    </div>
+    <AuthProvider>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="/todos" element={
+            <ProtectedRoute>
+              <Todos />
+            </ProtectedRoute>
+          } />
+          <Route path="/register" element={<RegisterForm />} />
+          <Route path="/login" element={<LoginForm />} />
+        </Route>
+      </Routes>
+    </AuthProvider>
   )
 }
 
